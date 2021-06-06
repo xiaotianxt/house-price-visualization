bd312299d51e59bbe88a04bd4da955682de3d325
xiaotianxt <1241811980@qq.com>
2021-06-06 23:22:18 +0800

    delete trivials files

--- back/config.py
+++ back/config.py
@@ -1,10 +1,9 @@
 '''
 Author: 小田
 Date: 2021-05-19 21:06:18
-LastEditTime: 2021-05-19 21:21:25
+LastEditTime: 2021-06-06 23:27:12
 '''
 import configparser
-
 
 class CONFIG():
     _CONFIG = configparser.ConfigParser()
